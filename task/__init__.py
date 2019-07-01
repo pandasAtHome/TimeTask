725008fc01335991e4f211824a24443237d41243
panda <2197884129@qq.com>
2019-07-01 10:34:55 +0800

    add request parameter parsing

--- task/__init__.py
+++ task/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-# task 包中添加新模块，需要在此处增加对应模块
-from task.test import Test
-from task.configs import Configs
+from mytools import glob
+from mytools import tools
 
 
 VERSION = 1.0
+
+# 请求参数解析：字典类型
+params = glob.get_value('params')
+if params:
+    params = tools.url_decode(params[0])
